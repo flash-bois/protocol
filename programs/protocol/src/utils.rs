67839c15df9a6842553a39802459342f5d08b1cd
tejks <sarmatys.programmer@gmail.com>
2023-02-11 16:51:32 +0100

    feat: base program

---
+++ programs/protocol/src/utils.rs
@@ -0,0 +1,20 @@
+use anchor_lang::__private::CLOSED_ACCOUNT_DISCRIMINATOR;
+use anchor_lang::prelude::*;
+use std::io::Write;
+
+pub fn close<'info>(info: AccountInfo<'info>, sol_destination: AccountInfo<'info>) -> Result<()> {
+    // Transfer tokens from the account to the sol_destination.
+    let dest_starting_lamports = sol_destination.lamports();
+    **sol_destination.lamports.borrow_mut() =
+        dest_starting_lamports.checked_add(info.lamports()).unwrap();
+    **info.lamports.borrow_mut() = 0;
+
+    // Mark the account discriminator as closed.
+    let mut data = info.try_borrow_mut_data()?;
+    let dst: &mut [u8] = &mut data;
+    let mut cursor = std::io::Cursor::new(dst);
+    cursor
+        .write_all(&CLOSED_ACCOUNT_DISCRIMINATOR)
+        .map_err(|_| ErrorCode::AccountDidNotSerialize)?;
+    Ok(())
+}
