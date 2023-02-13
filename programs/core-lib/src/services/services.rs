b591ee3e0732dc4f3534de31cc78ae82e9ce7e1e
Mateusz Zając <matzayonc@gmail.com>
2023-02-13 23:36:07 +0100

    services directory
    Co-authored-by: Tommy <tommysr@users.noreply.github.com>

---
+++ programs/core-lib/src/services/services.rs
@@ -0,0 +1,41 @@
+use super::lending::Lend;
+use crate::decimal::*;
+use crate::structs::Oracle;
+
+pub trait ServiceUpdate {
+    /// Increases available liquidity, and calculates new utilization.
+    fn add_available(&mut self, quantity: Quantity);
+
+    /// Decreases available liquidity, and calculates new utilization.
+    fn remove_available(&mut self, quantity: Quantity);
+
+    /// Returns amount of liquidity available in a service (sum of all strategies).
+    fn available(&self) -> Quantity;
+
+    /// Returns amount of used liquidity (like borrowed or sold).
+    /// The fees are be included only if the `accrue_fee` method was called before
+    fn locked(&self) -> Quantity;
+
+    /// Returns fees that are to be distributed to strategies and resets it.
+    fn accrue_fee(&mut self, oracle: Option<&Oracle>) -> Quantity;
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct Services {
+    pub lend: Option<Lend>,
+}
+
+pub enum ServiceType {
+    /// Lending allows borrowing of tokens
+    Lend,
+    /// Swapping allows swapping tokens between each other
+    Swap,
+    /// Trading allows creating leveraged LONG and SHORT positions
+    Trade,
+}
+
+impl Services {
+    pub fn lend_service(&mut self) -> Result<&mut Lend, ()> {
+        self.lend.as_mut().ok_or(())
+    }
+}
