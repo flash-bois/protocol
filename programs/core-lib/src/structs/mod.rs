8114ef7fff49bb38bf7adc2757e8e1b380205346
Mateusz Zając <matzayonc@gmail.com>
2023-02-18 21:20:33 +0100

    receipt type for position

--- programs/core-lib/src/structs/mod.rs
+++ programs/core-lib/src/structs/mod.rs
@@ -1,7 +1,9 @@
 pub mod fee_curve;
 pub mod fixed_vector;
 pub mod oracle;
+mod receipt;
 
 pub use fee_curve::FeeCurve;
 pub use fixed_vector::FixedSizeVector;
 pub use oracle::{Oracle, OraclePriceType};
+pub use receipt::{Receipt, Side};
---
+++ programs/core-lib/src/structs/receipt.rs
@@ -0,0 +1,22 @@
+use crate::decimal::{FundingRate, Price, Quantity, Value};
+
+#[repr(u8)]
+#[derive(Clone, Copy, Debug)]
+pub enum Side {
+    Long,
+    Short,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Receipt {
+    /// side of the position: either long or short
+    pub side: Side,
+    /// size of the position (in base token)
+    pub size: Quantity,
+    /// quantity of tokens locked in the position (size for LONG)
+    pub locked: Quantity,
+    /// price at which the position was opened
+    pub open_price: Price,
+    /// value o position at the moment of creation
+    pub open_value: Value,
+}
