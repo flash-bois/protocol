19a75bf19b710f5d4f80ae64832ea1f6c6126e05
Mateusz Zając <matzayonc@gmail.com>
2023-02-22 19:23:22 +0100

    everything

--- programs/protocol/src/instructions/create_state.rs
+++ programs/protocol/src/instructions/create_state.rs
@@ -1,11 +1,12 @@
 use crate::structs::{State, Vaults};
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
 #[derive(Accounts)]
 #[instruction( nonce: u8 )]
 pub struct CreateState<'info> {
-    #[account(init, seeds = [b"state".as_ref()], bump, payer = admin, space = 8 + 97)]
-    pub state: AccountLoader<'info, State>,
+    // #[account(init, seeds = [b"state".as_ref()], bump, payer = admin, space = 8 + 97)]
+    // pub state: AccountLoader<'info, State>,
     #[account(seeds = [b"DotWave".as_ref()], bump = nonce)]
     /// CHECK:
     pub program_authority: AccountInfo<'info>,
@@ -14,29 +15,31 @@ pub struct CreateState<'info> {
     #[account(zero)]
     pub vaults: AccountLoader<'info, Vaults>,
     pub rent: Sysvar<'info, Rent>,
-    pub system_program: SystemAccount<'info>,
+    /// CHECK:
+    #[account(address = system_program::ID)]
+    pub system_program: AccountInfo<'info>,
 }
 
 impl<'info> CreateState<'info> {
-    pub fn handler(&self, bump: u8, nonce: u8) -> Result<()> {
-        let state = &mut self.state.load_init()?;
-        let vaults = &mut self.vaults.load_init()?;
+    pub fn handler(&self, nonce: u8) -> Result<()> {
+        // let state = &mut self.state.load_init()?;
+        // let vaults = &mut self.vaults.load_init()?;
 
         msg!("Initializing state");
 
-        **state = State {
-            authority: *self.program_authority.key,
-            admin: *self.admin.key,
-            vaults: *self.vaults.to_account_info().key,
-            nonce,
-            bump,
-        };
+        // **state = State {
+        //     authority: *self.program_authority.key,
+        //     admin: *self.admin.key,
+        //     vaults: *self.vaults.to_account_info().key,
+        //     nonce,
+        //     bump,
+        // };
 
         msg!("Initializing vaults");
 
-        **vaults = Vaults {
-            ..Default::default()
-        };
+        // **vaults = Vaults {
+        //     ..Default::default()
+        // };
 
         Ok(())
     }
--- tests/anchor/create_state.spec.ts
+++ tests/anchor/create_state.spec.ts
@@ -1,68 +1,71 @@
 import * as anchor from '@coral-xyz/anchor'
-import { Provider, BN, Program, utils } from '@coral-xyz/anchor'
-import { Keypair, PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, Transaction } from '@solana/web3.js'
+import { Provider, BN, Program, } from '@coral-xyz/anchor'
+import { Connection, Keypair, PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, Transaction } from '@solana/web3.js'
 import { Protocol } from '../../target/types/protocol'
-
 const SEED = "DotWave"
 const STATE_SEED = "state"
 
 describe('state with default vaults', () => {
-  it('creates state', async () => {
-    const program = anchor.workspace.Protocol as Program<Protocol>
+  const provider = anchor.AnchorProvider.env()
+  const program = anchor.workspace.Protocol as Program<Protocol>
+
+  const admin = Keypair.generate()
+  const vaults = Keypair.generate()
+  const vaults_size = program.account.vaults.size;
+
+  anchor.setProvider(provider)
+
+  const [programAuthority, nonce] = PublicKey.findProgramAddressSync(
+    [Buffer.from(SEED)],
+    program.programId
+  )
+
+  const [state_address, bump] = PublicKey.findProgramAddressSync(
+    [Buffer.from(anchor.utils.bytes.utf8.encode(STATE_SEED))],
+    program.programId
+  )
+
+  it('Creates states!', async () => {
+    let tx = new Transaction();
 
 
-    const admin = Keypair.generate()
-    const vaults = Keypair.generate()
-    const vaults_size = program.account.vaults.size
-    anchor.setProvider(anchor.AnchorProvider.local())
-
-    const [programAuthority, nonce] = PublicKey.findProgramAddressSync(
-      [Buffer.from(SEED)],
-      program.programId
-    )
-
-    const [state_address, bump] = PublicKey.findProgramAddressSync(
-      [Buffer.from(utils.bytes.utf8.encode(STATE_SEED))],
-      program.programId
-    )
-
-
-    await program.provider.connection.requestAirdrop(admin.publicKey, 10000000000)
-    await new Promise(f => setTimeout(f, 6000));
-
-
-
-    // let pre_ix = SystemProgram.createAccount({
+    // let create_state_account_tx = SystemProgram.createAccount({
     //   fromPubkey: admin.publicKey,
     //   newAccountPubkey: vaults.publicKey,
     //   space: vaults_size,
-    //   lamports: await program.provider.connection.getMinimumBalanceForRentExemption(
+    //   lamports: await provider.connection.getMinimumBalanceForRentExemption(
     //     vaults_size
     //   ),
-    //   programId: program.programId
+    //   programId: SystemProgram.programId
     // })
+
+    // tx.add(create_state_account_tx)
+
+    tx.add(await program.account.vaults.createInstruction(vaults))
 
     let create_state_tx = await program.methods.createState(nonce).accounts({
       admin: admin.publicKey,
-      state: state_address,
+      // state: state_address,
       programAuthority: programAuthority,
       rent: SYSVAR_RENT_PUBKEY,
       systemProgram: SystemProgram.programId,
       vaults: vaults.publicKey
-    }).signers([admin]).rpc()
+    }).instruction()
+    tx.add(create_state_tx)
 
-    // tx.add(create_state_tx)
-    // let blockhash = (await provider.connection.getLatestBlockhash('recent')).blockhash;
-    // tx.recentBlockhash = blockhash;
-    // tx.feePayer = admin.publicKey
+    const blockhash = await provider.connection.getRecentBlockhash()
+    tx.recentBlockhash = blockhash.blockhash
 
-    // const signers: Keypair[] = [vaults, admin]
-    // tx.partialSign(...signers)
-    // const raw_tx = tx.serialize()
+    await anchor.getProvider().connection.requestAirdrop(admin.publicKey, 1000000000)
+    await new Promise(r => setTimeout(r, 1000))
 
+    tx.feePayer = admin.publicKey
 
+    const signers: Keypair[] = [ admin, vaults  ]
+    tx.partialSign(...signers)
+    const raw_tx = tx.serialize()
 
-    // await provider.connection.sendRawTransaction(raw_tx)
+    await provider.connection.sendRawTransaction(raw_tx)
 
     // const[userStatsPDA, _] = await PublicKey.findProgramAddress(
     //   [
@@ -73,6 +76,4 @@ describe('state with default vaults', () => {
     // )
   })
 })
-
-
 
