fa53823ac1053ffb38b1087b39975ed28ccc0ccf
tommysr <47206288+tommysr@users.noreply.github.com>
2023-03-13 00:47:56 +0100

    deploy script, check if state exists

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,9 +3,10 @@ import { Program, BN } from '@coral-xyz/anchor'
 import { clusterApiUrl, PublicKey, sendAndConfirmTransaction, Signer } from '@solana/web3.js'
 
 import { Protocol } from '../target/types/protocol'
-import { admin, minter } from '../microSdk'
-import { createDevnetEnvironment, IModifyCurveType, createStateWithVaults, IStateWithVaults } from '../tests/utils/utils'
+import { admin, minter, STATE_SEED } from '../microSdk'
+import { createDevnetEnvironment, IModifyCurveType, createStateWithVaults, IStateWithVaults, waitFor, sleep } from '../tests/utils/utils'
 import { createMint } from '@solana/spl-token'
+import { StateAccount } from '../pkg/protocol'
 
 anchor.setProvider(
   anchor.AnchorProvider.local(clusterApiUrl('devnet'), {
@@ -21,15 +22,42 @@ const wallet: Signer = provider.wallet.payer
 
 const connection = program.provider.connection
 
+
+const [state, bump] = PublicKey.findProgramAddressSync(
+  [Buffer.from(anchor.utils.bytes.utf8.encode(STATE_SEED))],
+  program.programId
+)
+
 const main = async () => {
+  console.log('creating state with vaults')
+
+  const acc_info = await connection.getAccountInfo(state)
+  let state_with_vault: IStateWithVaults;
+
+  if (acc_info == undefined) {
+    state_with_vault = await createStateWithVaults({ admin, ix_only: false, program }) as IStateWithVaults
+
+  } else {
+    const state_acc = StateAccount.load(acc_info?.data!)
+
+    state_with_vault = {
+      state: new PublicKey(state),
+      vaults: new PublicKey(state_acc.get_vaults_account())
+    }
+  }
+
+  console.log('created state with vaults')
+
   await connection.requestAirdrop(admin.publicKey, 1000000000)
+
+  console.log('airdrop done')
 
   const quote_mint = await createMint(connection, admin, minter.publicKey, null, 6)
   const base_mint1 = await createMint(connection, admin, minter.publicKey, null, 6)
   const base_mint2 = await createMint(connection, admin, minter.publicKey, null, 6)
   const base_mint3 = await createMint(connection, admin, minter.publicKey, null, 6)
   const base_mint4 = await createMint(connection, admin, minter.publicKey, null, 6)
-  const state_with_vault = await createStateWithVaults({ admin, ix_only: false, program }) as IStateWithVaults
+
 
   const vault_txs = await createDevnetEnvironment({
     ix_only: true,
@@ -458,10 +486,21 @@ const main = async () => {
 
   while (i < len) {
     try {
-      sendAndConfirmTransaction(provider.connection, vault_txs[i], [admin], { 'skipPreflight': true })
+
+      console.log('creating vault ' + i)
+
+      const sig = await sendAndConfirmTransaction(provider.connection, vault_txs[i], [admin], { 'skipPreflight': true })
+
+      await waitFor(provider.connection, sig)
     } catch (err) {
+      console.log(err)
+      console.log('sleeping for 2000')
+      await sleep(2000)
       continue;
     }
+
+    console.log('created vault ' + i)
+
 
     i++
   }
