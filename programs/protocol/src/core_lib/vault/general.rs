ca76d54082a05d2554c7087db66276041df537b8
tommysr <47206288+tommysr@users.noreply.github.com>
2023-03-14 22:53:28 +0100

    withdraw/deposit/lock/unlock test

--- programs/protocol/src/core_lib/vault/general.rs
+++ programs/protocol/src/core_lib/vault/general.rs
@@ -279,3 +279,329 @@ impl Vault {
         )
     }
 }
+
+#[cfg(test)]
+mod tests_general {
+    use super::*;
+    use crate::core_lib::{
+        decimal::{Balances, DecimalPlaces, Fraction, Price, Shares, Utilization},
+        structs::FeeCurve,
+        Token,
+    };
+    use checked_decimal_macro::{Decimal, Factories};
+
+    fn test_vault() -> Result<Vault, LibErrors> {
+        let mut vault = Vault::default();
+
+        vault.enable_oracle(
+            DecimalPlaces::Six,
+            Price::from_integer(2),
+            Price::from_scale(5, 3),
+            Price::from_scale(2, 2),
+            0,
+            Token::Base,
+            0,
+        )?;
+
+        vault.enable_oracle(
+            DecimalPlaces::Six,
+            Price::from_integer(1),
+            Price::from_scale(1, 3),
+            Price::from_scale(2, 2),
+            0,
+            Token::Quote,
+            0,
+        )?;
+
+        vault.enable_lending(
+            FeeCurve::default(),
+            Utilization::from_integer(1),
+            Quantity::new(u64::MAX),
+            0,
+            0,
+        )?;
+
+        vault.enable_swapping(
+            FeeCurve::default(),
+            FeeCurve::default(),
+            Fraction::from_scale(1, 1),
+        )?;
+
+        vault.enable_trading(
+            Fraction::new(100),
+            Fraction::from_integer(3),
+            Fraction::from_integer(1),
+            Fraction::from_integer(1),
+            0,
+        )?;
+
+        vault.add_strategy(
+            true,
+            true,
+            true,
+            Fraction::from_integer(1),
+            Fraction::from_integer(1),
+        )?;
+
+        vault.add_strategy(
+            true,
+            false,
+            true,
+            Fraction::from_integer(1),
+            Fraction::from_integer(1),
+        )?;
+
+        vault.add_strategy(
+            false,
+            true,
+            true,
+            Fraction::from_integer(1),
+            Fraction::from_integer(1),
+        )?;
+
+        Ok(vault)
+    }
+
+    #[test]
+    fn deposit() -> Result<(), LibErrors> {
+        let mut vault = test_vault()?;
+
+        let base = Quantity::new(2000000000);
+        let quote = Quantity::new(30000000000);
+        let shares = Shares::new(1000);
+        let balances = Balances { base, quote };
+        let zero_balances = Balances {
+            quote: Quantity::new(0),
+            base: Quantity::new(0),
+        };
+
+        vault
+            .strategies
+            .get_strategy_mut(0)?
+            .deposit(base, quote, shares, &mut vault.services);
+
+        let lend = vault.lend_service_not_mut()?;
+        let trade = vault.trade_service_not_mut()?;
+        let swap = vault.swap_service_not_mut()?;
+        let strategy = vault.strategies.get_strategy(0)?;
+
+        assert_eq!(lend.available, base);
+        assert_eq!(trade.available, balances);
+        assert_eq!(swap.available, balances);
+        assert_eq!(swap.balances, balances);
+        assert_eq!(strategy.total_shares, shares);
+        assert_eq!(strategy.available, balances);
+        assert_eq!(strategy.locked, zero_balances);
+        assert_eq!(strategy.accrued_fee, Quantity::new(0));
+
+        vault
+            .strategies
+            .get_strategy_mut(1)?
+            .deposit(base, quote, shares, &mut vault.services);
+
+        let lend = vault.lend_service_not_mut()?;
+        let trade = vault.trade_service_not_mut()?;
+        let swap = vault.swap_service_not_mut()?;
+        let strategy = vault.strategies.get_strategy(1)?;
+
+        assert_eq!(lend.available, base + base);
+        assert_eq!(trade.available, balances + balances);
+        assert_eq!(swap.available, balances);
+        assert_eq!(swap.balances, balances);
+        assert_eq!(strategy.total_shares, shares);
+        assert_eq!(strategy.available, balances);
+        assert_eq!(strategy.locked, zero_balances);
+
+        Ok(())
+    }
+
+    #[test]
+    fn withdraw() -> Result<(), LibErrors> {
+        let mut vault = test_vault()?;
+
+        let base = Quantity::new(2000000000);
+        let quote = Quantity::new(30000000000);
+        let shares = Shares::new(1000);
+        let balances = Balances { base, quote };
+        let zero_balances = Balances {
+            quote: Quantity::new(0),
+            base: Quantity::new(0),
+        };
+        let zero_quantity = Quantity::new(0);
+
+        vault
+            .strategies
+            .get_strategy_mut(0)?
+            .deposit(base, quote, shares, &mut vault.services);
+
+        vault
+            .strategies
+            .get_strategy_mut(1)?
+            .deposit(base, quote, shares, &mut vault.services);
+
+        vault
+            .strategies
+            .get_strategy_mut(0)?
+            .withdraw(base, quote, shares, &mut vault.services);
+
+        let lend = vault.lend_service_not_mut()?;
+        let trade = vault.trade_service_not_mut()?;
+        let swap = vault.swap_service_not_mut()?;
+        let strategy = vault.strategies.get_strategy(0)?;
+
+        assert_eq!(lend.available, base);
+        assert_eq!(trade.available, balances);
+        assert_eq!(swap.available, zero_balances);
+        assert_eq!(swap.balances, zero_balances);
+        assert_eq!(strategy.total_shares, Shares::new(0));
+        assert_eq!(strategy.available, zero_balances);
+        assert_eq!(strategy.locked, zero_balances);
+        assert_eq!(strategy.accrued_fee, Quantity::new(0));
+
+        vault
+            .strategies
+            .get_strategy_mut(1)?
+            .withdraw(base, quote, shares, &mut vault.services);
+
+        let lend = vault.lend_service_not_mut()?;
+        let trade = vault.trade_service_not_mut()?;
+        let swap = vault.swap_service_not_mut()?;
+        let strategy = vault.strategies.get_strategy(1)?;
+
+        assert_eq!(lend.available, zero_quantity);
+        assert_eq!(trade.available, zero_balances);
+        assert_eq!(swap.available, zero_balances);
+        assert_eq!(swap.balances, zero_balances);
+        assert_eq!(strategy.total_shares, Shares::new(0));
+        assert_eq!(strategy.available, zero_balances);
+        assert_eq!(strategy.locked, zero_balances);
+        assert_eq!(strategy.accrued_fee, Quantity::new(0));
+
+        Ok(())
+    }
+
+    #[test]
+    fn lock_base() -> Result<(), LibErrors> {
+        let mut vault = test_vault()?;
+
+        let base = Quantity::new(2000000000);
+        let quote = Quantity::new(30000000000);
+        let shares = Shares::new(1000);
+        let balances = Balances { base, quote };
+        let zero_balances = Balances {
+            quote: Quantity::new(0),
+            base: Quantity::new(0),
+        };
+        let zero_quantity = Quantity::new(0);
+
+        vault
+            .strategies
+            .get_strategy_mut(0)?
+            .deposit(base, quote, shares, &mut vault.services);
+
+        vault
+            .strategies
+            .get_strategy_mut(1)?
+            .deposit(base, quote, shares, &mut vault.services);
+
+        vault
+            .strategies
+            .get_strategy_mut(2)?
+            .deposit(base, quote, shares, &mut vault.services);
+
+        let swap = vault.swap_service_not_mut()?;
+
+        let lock_base = Quantity::new(3000000000);
+        assert_eq!(swap.available.base, Quantity::new(4000000000));
+
+        vault.lock_base(lock_base, swap.available.base, ServiceType::Swap)?;
+
+        let lend = vault.lend_service_not_mut()?;
+        let trade = vault.trade_service_not_mut()?;
+        let swap = vault.swap_service_not_mut()?;
+        let strategy = vault.strategies.get_strategy(0)?;
+
+        let locked = Balances {
+            quote: zero_quantity,
+            base: Quantity::new(1500000000),
+        };
+
+        assert_eq!(strategy.available, balances - locked);
+        assert_eq!(strategy.locked, locked);
+        assert_eq!(strategy.accrued_fee, Quantity::new(0));
+
+        let strategy = vault.strategies.get_strategy(1)?;
+
+        assert_eq!(strategy.available, balances);
+        assert_eq!(strategy.locked, zero_balances);
+        assert_eq!(strategy.accrued_fee, Quantity::new(0));
+
+        let strategy = vault.strategies.get_strategy(2)?;
+
+        assert_eq!(strategy.available, balances - locked);
+        assert_eq!(strategy.locked, locked);
+        assert_eq!(strategy.accrued_fee, Quantity::new(0));
+
+        assert_eq!(swap.available, balances + balances - locked - locked);
+        assert_eq!(swap.balances, balances + balances);
+        assert_eq!(lend.available, base + base - locked.base);
+        assert_eq!(
+            trade.available,
+            balances + balances + balances - locked - locked
+        );
+
+        vault.unlock_base(lock_base, lock_base, ServiceType::Swap)?;
+
+        let lend = vault.lend_service_not_mut()?;
+        let trade = vault.trade_service_not_mut()?;
+        let swap = vault.swap_service_not_mut()?;
+        let strategy = vault.strategies.get_strategy(0)?;
+
+        assert_eq!(strategy.available, balances);
+        assert_eq!(strategy.locked, zero_balances);
+        assert_eq!(strategy.accrued_fee, Quantity::new(0));
+
+        let strategy = vault.strategies.get_strategy(1)?;
+
+        assert_eq!(strategy.available, balances);
+        assert_eq!(strategy.locked, zero_balances);
+        assert_eq!(strategy.accrued_fee, Quantity::new(0));
+
+        let strategy = vault.strategies.get_strategy(2)?;
+
+        assert_eq!(strategy.available, balances);
+        assert_eq!(strategy.locked, zero_balances);
+        assert_eq!(strategy.accrued_fee, Quantity::new(0));
+
+        assert_eq!(swap.available, balances + balances);
+        assert_eq!(swap.balances, balances + balances);
+        assert_eq!(lend.available, base + base);
+        assert_eq!(trade.available, balances + balances + balances);
+
+        Ok(())
+    }
+}
+
+// vault.add_strategy(
+//     true,
+//     true,
+//     true,
+//     Fraction::from_integer(1),
+//     Fraction::from_integer(1),
+// )?;
+
+// vault.add_strategy(
+//     true,
+//     false,
+//     true,
+//     Fraction::from_integer(1),
+//     Fraction::from_integer(1),
+// )?;
+
+// vault.add_strategy(
+//     false,
+//     true,
+//     true,
+//     Fraction::from_integer(1),
+//     Fraction::from_integer(1),
+// )?;
